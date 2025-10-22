21edbb89e8ba0423ed2eb5e04541b04aeeb9ac67
user <mallaev.ra@phystech.edu>
2025-10-22 18:34:50 +0300

    refactor: ruff

--- artemis/modules/allostery/criticality.py
+++ artemis/modules/allostery/criticality.py
@@ -1,28 +1,33 @@
-import json
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
-import pandas as pd
-from scipy.stats import zscore
 
-width = .6
+width = 0.6
+
 
 def search_critical(Allostery, out_path, noseq):
-
     print("\nSCRIPT FOR ALLOSTERIC COMMUNICATION INTENSITY CALCULATION IS LAUNCHED\n")
 
     mask = np.ones(Allostery.NResidues)
     for resid in Allostery.active_site:
-        for i in range(int(max(0, resid-1-noseq)), int(min(Allostery.NResidues, resid+noseq))):
+        for i in range(
+            int(max(0, resid - 1 - noseq)), int(min(Allostery.NResidues, resid + noseq))
+        ):
             mask[i] = 0
 
     for resid in Allostery.allosteric_site:
-        for i in range(int(max(0, resid-1-noseq)), int(min(Allostery.NResidues, resid+noseq))):
+        for i in range(
+            int(max(0, resid - 1 - noseq)), int(min(Allostery.NResidues, resid + noseq))
+        ):
             mask[i] = 0
 
     intensity = []
     for i in range(Allostery.NResidues):
-        if i + 1 in Allostery.active_site or mask[i] == 0 or i + 1 in Allostery.allosteric_site:
+        if (
+            i + 1 in Allostery.active_site
+            or mask[i] == 0
+            or i + 1 in Allostery.allosteric_site
+        ):
             intensity.append(0)
         else:
             mi_act = 0
@@ -32,42 +37,56 @@ def search_critical(Allostery, out_path, noseq):
                 mi_act += Allostery.map_[resid - 1][i]
             for resid in Allostery.allosteric_site:
                 mi_all += Allostery.map_[resid - 1][i]
-            intensity.append(mi_all*mi_act/entr)
+            intensity.append(mi_all * mi_act / entr)
 
     new_names = []
     for i in range(Allostery.NResidues):
-        new_names.append(Allostery.names[i] + "\n(" + str(Allostery.real_numbers[i]) +")")
+        new_names.append(
+            Allostery.names[i] + "\n(" + str(Allostery.real_numbers[i]) + ")"
+        )
 
     unique, counts = np.unique(new_names, return_counts=True)
 
     for i, item in enumerate(unique):
         if counts[i] > 1:
-            indeces = np.argwhere(np.array(new_names)==item)
+            indeces = np.argwhere(np.array(new_names) == item)
             for j, idx in enumerate(indeces):
-                new_names[int(idx[0])] += "[" +str(j+1)+"]"
+                new_names[int(idx[0])] += "[" + str(j + 1) + "]"
 
     Allostery.intensity = np.array(intensity)
 
     INTENSITY = {}
-    INTENSITY["Intensity"] = np.array(intensity) / np.sqrt(sum(abs(np.array(intensity).flatten())**2))
+    INTENSITY["Intensity"] = np.array(intensity) / np.sqrt(
+        sum(abs(np.array(intensity).flatten()) ** 2)
+    )
     INTENSITY["Residue"] = new_names
 
-    colors = plt.cm.viridis(INTENSITY["Intensity"]/np.max(INTENSITY["Intensity"]))
+    colors = plt.cm.viridis(INTENSITY["Intensity"] / np.max(INTENSITY["Intensity"]))
     colors = (sns.color_palette(colors, as_cmap=True)).tolist()
 
-    fig, axs = plt.subplots(figsize=(Allostery.NResidues*width, 20), constrained_layout=True)
-    axs = sns.barplot(x="Residue", y="Intensity", data=INTENSITY, palette=colors, dodge=False, hue="Residue", legend=False)
-    plt.tick_params(axis='both', which='major', labelsize=16)
+    fig, _ = plt.subplots(
+        figsize=(Allostery.NResidues * width, 20), constrained_layout=True
+    )
+    sns.barplot(
+        x="Residue",
+        y="Intensity",
+        data=INTENSITY,
+        palette=colors,
+        dodge=False,
+        hue="Residue",
+        legend=False,
+    )
+    plt.tick_params(axis="both", which="major", labelsize=16)
 
-    plt.title('Intensity of connectivity of residues with the active site', fontsize=40)
+    plt.title("Intensity of connectivity of residues with the active site", fontsize=40)
     try:
         fig.savefig(out_path)
-        print("File",out_path + " created")
-    except:
-        print("Error writing file",out_path)
+        print("File", out_path + " created")
+    except Exception as e:
+        print(f"Error writing file {out_path}: {e}\n")
 
     if out_path == "allosteric_intensity.pdf":
         fname = "allostery.json"
     else:
-        fname = ''.join(out_path.split('.')[:-1]) + '.json'
+        fname = "".join(out_path.split(".")[:-1]) + ".json"
     Allostery.write(fname)
